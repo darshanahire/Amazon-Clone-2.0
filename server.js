9846359d5a500d47f28b857961130b228ea45d3d
Darshan Ahire <72602965+darshanahire@users.noreply.github.com>
2023-08-05 09:35:58 +0530

    deployment commit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(router)
 
 app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
 })
 
 app.listen(PORT, () => {
